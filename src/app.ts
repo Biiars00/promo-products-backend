1dffd62225a27eac7d0aeb279dfc2553c7807e97
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2025-06-19 23:36:43 -0300

    feat: middleware configuration, tsoa routes and swagger.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import 'reflect-metadata';
 import cors from 'cors';
 import express, { Application } from 'express';
+import { RegisterRoutes } from './routes/routes';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './docs/swagger.json';
+import './containers/dependencies';
 import dotenv from 'dotenv';
 import { corsConfig } from './config/cors';
+import { notFoundErrorMiddleware } from './middlewares/notFoundError.middleware';
+import { internalServerErrorMiddleware } from './middlewares/internalServerError.middleware';
 
 dotenv.config();
 
@@ -12,12 +18,19 @@ app.use(cors(corsConfig));
 
 app.use(express.json());
 
-app.get('/', (_req, res) => {
-    res.send('Bem-vindo à API ISI Products Server');
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/api/v1', (_req, res) => {
+  res.send('Bem-vindo à API ISI Products Server');
 });
 
-app.use((_req, res) => {
-    res.status(404).send({ status: 'Not Found!' });
-});
+const apiRouter = express.Router();
+
+RegisterRoutes(apiRouter);
+app.use('/api/v1', apiRouter);
+
+app.use(notFoundErrorMiddleware);
+
+app.use(internalServerErrorMiddleware);
 
 export default app;
