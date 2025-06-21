b6b1bc733773591b95cc5e5dcb8086ecaf389d31
Beatriz Ribeiro <biiaribeiro60@gmail.com>
2025-06-21 08:53:59 -0300

    feat: adding new dependencies.

--- src/containers/dependencies.ts
+++ src/containers/dependencies.ts
@@ -1,4 +1,7 @@
+import { checkCouponValidity } from './coupons/checkCouponValidity.dependencie';
+import { coupons } from './coupons/coupons.dependencie';
 import { products } from './products/products.dependencie';
 import { time } from './time/time.dependencie';
+import { validateStringRegex } from './validateStringRegex/validateStringRegex.dependencie';
 
-export { products, time as container };
+export { products, time, coupons, validateStringRegex, checkCouponValidity as container };
